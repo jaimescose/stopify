0b87dd9bf65f3c220d70668be9f33da2e0939f33
jjescobaruni <jjescobar@uninorte.edu.co>
2020-05-08 22:03:43 -0500

    small fix for daily script

--- daily_script.py
+++ daily_script.py
@@ -7,7 +7,7 @@ from models import User
 date = date.today()
 previous_date = date - timedelta(days=7)
 
-users = User.query.filter_by(is_active=True).filter(User.last_post_date == previous_date)
+users = User.query.filter_by(is_active=True).filter(User.last_post_date >= previous_date)
 
 for user in users:
     user.post_track_status(allow_check=False)
